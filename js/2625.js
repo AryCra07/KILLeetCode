f7cfa114c91eaa52a764d28b829dd2caeacce6f7
Yuanhao Shen <703810491@qq.com>
2023-09-02 00:18:10 +0800

    Delete js directory

--- js/2625.js
+++
@@ -1,9 +0,0 @@
-/**
- * @param {any[]} arr
- * @param {number} depth
- * @return {any[]}
- */
-var flat = function (arr, n) {
-    while (n-- > 0 && arr.some(Array.isArray)) arr = [].concat(...arr);
-    return arr;
-};
--- js/2632.js
+++
@@ -1,7 +0,0 @@
-var curry = function (fn) {
-    return function curried(...args) {
-        return args.length >= fn.length
-            ? fn.apply(this, args)
-            : (...newArgs) => curried.apply(this, args.concat(newArgs));
-    };
-};
